b8ba6b5f6c9243615cab83e491b4f7d54712cc2d
izaphod <dugarov.nima@gmail.com>
2021-08-25 20:46:42 +0700

    Fix: appContext injected to HomePresenter.kt, args passed to DetailsPresenter.kt

--- app/src/main/java/com/nimadugarov/testappsequenia/domain/model/MovieItem.kt
+++ app/src/main/java/com/nimadugarov/testappsequenia/domain/model/MovieItem.kt
@@ -1,8 +1,10 @@
 package com.nimadugarov.testappsequenia.domain.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
+@Keep
 @Parcelize
 data class MovieItem(
     val movieId: Long,
--- app/src/main/java/com/nimadugarov/testappsequenia/presentation/details/DetailsPresenter.kt
+++ app/src/main/java/com/nimadugarov/testappsequenia/presentation/details/DetailsPresenter.kt
@@ -1,11 +1,25 @@
 package com.nimadugarov.testappsequenia.presentation.details
 
+import com.nimadugarov.testappsequenia.domain.model.MovieItem
+import moxy.InjectViewState
 import moxy.MvpPresenter
 
+@InjectViewState
 class DetailsPresenter : MvpPresenter<DetailsView>() {
+
+    var movieItem: MovieItem? = null
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
-        viewState.showMovieDetails()
+        movieItem?.let {
+            viewState.setToolbarTitle(it.localizedName)
+            viewState.setMoviePoster(it.posterUrl)
+            viewState.setMovieDetails(
+                name = it.name,
+                year = it.year.toString(),
+                rating = it.rating,
+                description = it.description
+            )
+        }
     }
 }
